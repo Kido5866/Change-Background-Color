49cfb5ae8e45a65c44b545f054178b387e186a45
Kido5866 <76984004+Kido5866@users.noreply.github.com>
2021-02-07 10:38:07 +0800

    Add files via upload

--- Change Background Colour/script.js
+++ Change Background Colour/script.js
@@ -1,6 +1,6 @@
 const colorBtn = document.querySelector('.colorBtn');
 const bodyBcg = document.querySelector('body');
-const colors = ['yellow','red', 'green, '#3b5998'];
+const colors = ['yellow','red', 'green', "#3b5998"];
 colorBtn.addEventListener('click', changeColor);
 function changeColor (){
     bodyBcg.style.backgroundColor = colors[2];
